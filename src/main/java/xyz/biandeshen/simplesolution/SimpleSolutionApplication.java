c76291c9a8dceda44c82f4eff01da1ebb5986e30
biandeshen <biandeshen@gmail.com>
2020-11-18 15:42:17 +0800

    create: 创建项目并合并远程本地

---
+++ src/main/java/xyz/biandeshen/simplesolution/SimpleSolutionApplication.java
@@ -0,0 +1,13 @@
+package xyz.biandeshen.simplesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSolutionApplication.class, args);
+	}
+
+}

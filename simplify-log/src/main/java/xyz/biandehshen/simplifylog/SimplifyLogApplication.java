0bab127c3e153896a3580a3b98f9c3ac277eda43
biandeshen <biandeshen@gmail.com>
2020-11-19 16:38:14 +0800

    create: 创建并完成入口参数日志注解项目以及创建并完成注解限流（含ip）项目

---
+++ simplify-log/src/main/java/xyz/biandehshen/simplifylog/SimplifyLogApplication.java
@@ -0,0 +1,13 @@
+package xyz.biandehshen.simplifylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifyLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifyLogApplication.class, args);
+	}
+
+}
---
+++ simpllify-limiting/src/main/java/xyz/biandeshen/simpllifylimiting/SimpllifyLimitingApplication.java
@@ -0,0 +1,13 @@
+package xyz.biandeshen.simpllifylimiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpllifyLimitingApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SimpllifyLimitingApplication.class, args);
+	}
+	
+}
